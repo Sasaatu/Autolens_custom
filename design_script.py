1bb8831569e02bbfa30d247c9b159db5530e5074
Sasaatu <sasaatu93618frc@gmail.com>
2025-12-02 15:25:20 +0900

    minor changes for design_script

--- design_script.py
+++ design_script.py
@@ -1,11 +1,11 @@
 """ Automated lens design with curriculum learning, using RMS errors as loss function.
 """
-import numpy as np
+import math
 import logging
 from auto_lens_design import default_inputs, define_lens, config, curriculum_learning
 
 if __name__ == '__main__':
-    args = default_inputs
+    args = default_inputs()
     
     # custom parmeters
     epd = 0.5
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     Waves = [675]
     
     # update inputs
-    hfov_rad = np.deg2rad(fov) / 2
-    fnum = image_height/(2*epd*np.tan(hfov_rad))
+    hfov_rad = math.radians(fov) / 2
+    fnum = image_height/(2*epd*math.tan(hfov_rad))
     rff = total_distance/image_height
     args['HFOV'] = hfov_rad
     args['FNUM'] = fnum
@@ -34,13 +34,14 @@ if __name__ == '__main__':
     # create lens
     lens = define_lens(args)
     # refine lens
-    lens = curriculum_learning(lens, args)
+    curriculum_learning(lens, args)
 
     # analyze final result
     lens.prune(outer=0.2)
     lens.post_computation()
 
     logging.info(f'Actual: FOV {lens.hfov}, IMGH {lens.r_last}, F/{lens.fnum}.')
-    lens.write_lensfile(f'{args['result_dir']}/final_lens.txt', write_zmx=True)
-    lens.write_lens_json(f'{args['result_dir']}/final_lens.json')
-    lens.analysis(save_name=f'{args['result_dir']}/final_lens', draw_layout=True)
+    result_dir = args['result_dir']
+    lens.write_lensfile(f'{result_dir}/final_lens.txt', write_zmx=True)
+    lens.write_lens_json(f'{result_dir}/final_lens.json')
+    lens.analysis(save_name=f'{result_dir}/final_lens', draw_layout=True)
