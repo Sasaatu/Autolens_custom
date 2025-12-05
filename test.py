e58ec30f39ea8fbf50f0f85e6126523f31ff66e9
Sasaatu <sasaatu93618frc@gmail.com>
2025-12-05 17:08:47 +0900

    rms calculation in test.py

--- test.py
+++ test.py
@@ -6,19 +6,21 @@ from deeplens.basics import DEPTH
 from auto_lens_design import default_inputs, config, design_lens
 
 if __name__ == '__main__':
-    M = 31
-    spp = 256
+    M = 9
+    spp = 64
     
     args = default_inputs()
     # custom parmeters
     Waves = [520]
-    iter = 2
-    iter_test = 2
-    iter_last = 2
-    iter_test_last = 2
+    num_step = 1
+    iter = 1
+    iter_test = 1
+    iter_last = 1
+    iter_test_last = 1
     
     # update inputs
     args['WAVES'] = Waves
+    args['curriculum_steps'] = num_step
     args['iter'] = iter
     args['iter_test'] = iter_test
     args['iter_last'] = iter_last
@@ -32,14 +34,16 @@ if __name__ == '__main__':
     # evaluate spot size
     mag = 1 / lens.calc_scale_pinhole(DEPTH)
     obj_r = lens.sensor_size[0]/2/mag
-    waves = lens.wave
-    ray = lens.sample_point_source(M=M,  spp=spp,  depth=DEPTH,  R=obj_r,  pupil=True, wavelength=waves)
+    wave = lens.wave[0]
+    ray = lens.sample_point_source(M=M,  spp=spp,  depth=DEPTH,  R=obj_r,  pupil=True, wavelength=wave)
     ray, _, _ = lens.trace(ray)
     xy = ray.project_to(lens.d_sensor)
     
     # plot spot diagram
     plt.figure()
-    plt.scatter(xy[:, 0], xy[:, 1], s=1)
+    for m in range(M):
+        xy_s = xy[:, m, m].detach().cpu().numpy().astype(float)
+        plt.scatter(xy_s[:, 0], xy_s[:, 1], s=1)
     plt.axis('equal')
     plt.title('Spot diagram')
     plt.xlabel('X [mm]')
